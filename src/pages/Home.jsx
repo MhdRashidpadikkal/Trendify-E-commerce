a59df80d3df6e4bbfde7da2cf67d8a9ac64dc0f0
Rashidprojects <rashidgitproject@gmail.com>
2025-03-25 13:35:33 +0530

    feature: user dynamic carousel created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/Hero'
+import UserCarousel from '@/components/UserCarousel'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <UserCarousel />
     </div>
   )
 }
