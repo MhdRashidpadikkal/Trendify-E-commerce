359d4aac6debbc6882eb35fea38f7465b93b9d3a
rashid <mhdrashid142@gmail.com>
2025-04-18 22:52:58 +0530

    feature : contact page ui completed

--- src/components/products/ProductFilters.jsx
+++ src/components/products/ProductFilters.jsx
@@ -17,20 +17,19 @@ const ProductFilters = () => {
     
     const {state, dispatch} = useFilterContext();
 
-    console.log("updated prices in context at filter hook", state.priceRangeMax + "min value is", state.priceRangeMin);
 
     useEffect(() => {
        const filtered = useCustomFilter(state.allProducts, selectedCategory, state.priceRangeMax, state.priceRangeMin, selectedRating, selectedBrand )
        dispatch({type: 'FILTER_PRODUCTS', payload: filtered})
        dispatch({ type: "SET_CURRENT_PAGE", payload: 1 });
-    console.log("filtered is", filtered)
+    
     }, [state.allProducts, selectedCategory, state.priceRangeMax, state.priceRangeMin, selectedRating, selectedBrand])
 
 
     return (
 
-        <div className={`md:w-[20%] md:border-2 bg-white border-r-2 rounded-md ${state.isFilterBar ? "block" : "hidden"} md:block px-5 absolute left-0 top-0 md:relative py-3 ${!state.showAllCategories ? "h-[750px]" : "h-[1010px]"} `}>
-            <CircleX onClick={() => dispatch({type:"HIDE_FILTER_BAR"})} className='absolute right-2 cursor-pointer md:hidden' />
+        <div className={`md:w-[20%] md:border-2 bg-white border-r-2 rounded-md ${state.isFilterBar ? "block" : "hidden"} md:block z-50 px-5 absolute left-0 top-0 md:relative py-3 ${!state.showAllCategories ? "h-[750px]" : "h-[1010px]"} `}>
+            <CircleX onClick={() => dispatch({type:"HIDE_FILTER_BAR"})} className='absolute cursor-pointer right-2 md:hidden' />
             <h3 className='text-xs text-gray-400'>Filter by Category</h3>
             <ul className='mt-3'>
                 {
@@ -49,9 +48,9 @@ const ProductFilters = () => {
                                         value={item}
                                         checked={selectedCategory.includes(item)}
                                         onChange={(e) => handleFilterState(e, setSelectedCategory)}
-                                        className="peer hidden"
+                                        className="hidden peer"
                                     />
-                                    <div className="w-4 h-4 rounded border border-gray-300 bg-white peer-checked:bg-green-500 peer-checked:border-green-600 flex items-center justify-center transition-colors duration-200">
+                                    <div className="flex items-center justify-center w-4 h-4 transition-colors duration-200 bg-white border border-gray-300 rounded peer-checked:bg-green-500 peer-checked:border-green-600">
                                         <Check className="h-[20px] w-[17px] text-white  " />
                                     </div>
                                     <span className="text-sm">{formatted}</span>
@@ -67,12 +66,12 @@ const ProductFilters = () => {
                 >view {!state.showAllCategories ? "more" : "less"}</button>
             </ul>
 
-            <h3 className='text-xs text-gray-400 mt-4 mb-2'>Price range</h3>
+            <h3 className='mt-4 mb-2 text-xs text-gray-400'>Price range</h3>
             <div>
                 <PriceRangeSlider />
             </div>
 
-            <h3 className='text-xs text-gray-400 mt-4 mb-2 '>Filter by rating</h3>
+            <h3 className='mt-4 mb-2 text-xs text-gray-400 '>Filter by rating</h3>
             {
                 [1, 2, 3, 4, 5].map((i) => (
                     <label className="flex items-center gap-2 cursor-pointer">
@@ -81,9 +80,9 @@ const ProductFilters = () => {
                             value={i}
                             checked={selectedRating.includes(`${i}`)}
                             onChange={(e) => handleFilterState(e, setSelectedRating)}
-                            className="peer hidden"
+                            className="hidden peer"
                         />
-                        <div className="w-4 h-4 rounded border border-gray-300 bg-white peer-checked:bg-green-500 peer-checked:border-green-600 flex items-center justify-center transition-colors duration-200">
+                        <div className="flex items-center justify-center w-4 h-4 transition-colors duration-200 bg-white border border-gray-300 rounded peer-checked:bg-green-500 peer-checked:border-green-600">
                             <Check className="h-[20px] w-[17px] text-white  " />
                         </div>
                         <span className="text-sm"><RatingStars starData={i} /> </span>
@@ -91,7 +90,7 @@ const ProductFilters = () => {
                 ))
             }
 
-            <h3 className='text-xs text-gray-400 mt-4 mb-2 '>Filter by brand</h3>
+            <h3 className='mt-4 mb-2 text-xs text-gray-400 '>Filter by brand</h3>
             {
                 productsBrands.map((item) => (
                     <label className="flex items-center gap-2 cursor-pointer">
@@ -100,9 +99,9 @@ const ProductFilters = () => {
                             value={item}
                             checked={selectedBrand.includes(item)}
                             onChange={(e) => handleFilterState(e, setSelectedBrand)}
-                            className="peer hidden"
+                            className="hidden peer"
                         />
-                        <div className="w-4 h-4 rounded border border-gray-300 bg-white peer-checked:bg-green-500 peer-checked:border-green-600 flex items-center justify-center transition-colors duration-200">
+                        <div className="flex items-center justify-center w-4 h-4 transition-colors duration-200 bg-white border border-gray-300 rounded peer-checked:bg-green-500 peer-checked:border-green-600">
                             <Check className="h-[20px] w-[17px] text-white  " />
                         </div>
                         <span className="text-sm">{item} </span>
